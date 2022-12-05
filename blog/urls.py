92276abf7a68702ab349fb787d7547355c0e255a
SINHUK <park070404@gmail.com>
2022-12-05 13:05:38 +0900

    로그인한 사용자에게만 포스트 작성 페이지 열어주기

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # 이 부분을 채울 겁니다.
+    # 이 부분을 채울 겁니다!
+    path('create_post/', views.PostCreate.as_view()),
     path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
-    #path('<int:pk>/', views.single_post_page),
-    #path('', views.index),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,20 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import Post, Category, Tag
-
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from . models import Post, Category, Tag
 
 # Create your views here.
-class PostList(ListView):
+class PostList(ListView) :
     model = Post
-    ordering = '-pk'
 
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
-    # template_name = 'blog/index.html'
 
-
-class PostDetail(DetailView):
+# Post 상세 보기
+class PostDetail(DetailView) :
     model = Post
 
     def get_context_data(self, **kwargs):
@@ -25,12 +23,25 @@ class PostDetail(DetailView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
+# CreateView
+class PostCreate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
-def category_page(request, slug):
-    if slug == 'no_category':
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated:
+            form.instance.author = current_user
+            return super(PostCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
+
+
+def category_page(request, slug) :
+    if slug == 'no_category' :
         category = '미분류'
-        post_list = Post.objects.filter(category=None)
-    else:
+        post_lsit = Post.objects.filter(category=None)
+    else :
         category = Category.objects.get(slug=slug)
         post_list = Post.objects.filter(category=category)
 
@@ -38,35 +49,12 @@ def category_page(request, slug):
         request,
         'blog/post_list.html',
         {
-            'post_list': post_list,
+            'post_list': Post.objects.filter(category=category),
             'categories': Category.objects.all(),
             'no_category_post_count': Post.objects.filter(category=None).count(),
             'category': category,
         }
     )
-
-
-# def index(request):
-#    posts = Post.objects.all().order_by('-pk')
-
-#    return render(
-#        request,
-#        'blog/index.html',
-#        {
-#            'posts':posts,
-#        }
-#    )
-
-# def single_post_page(request, pk):
-#    post = Post.objects.get(pk=pk)
-#
-#    return render(
-#        request,
-#        'blog/single_post_page.html',
-#        {
-#            'post':post,
-#        }
-#    )
 
 def tag_page(request, slug):
     tag = Tag.objects.get(slug=slug)
