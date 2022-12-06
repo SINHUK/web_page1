263812348170d98a8863ea56a4a8f5ea14e521fc
SINHUK <park070404@gmail.com>
2022-12-06 11:42:40 +0900

    포스트를 수정할 때 테긋트로 태그 지정하기

--- blog/test.py
+++ blog/test.py
@@ -196,12 +196,12 @@ class TestView(TestCase) :
         response = self.client.get('/blog/create_post/')
         self.assertNotEqual(response.status_code, 200)
 
-        # staff가 아닌 trump가 로그인 한다.
+        # sfaff가 아닌 trump가 로그인을 한다.
         self.client.login(username='trump', password='somepassword')
         response = self.client.get('/blog/create_post')
         self.assertNotEqual(response.status_code, 200)
 
-        # staff인 obama로 로그인 한다.
+        # staff인 obama로 로그인한다.
         self.client.login(username='obama', password='somepassword')
         response = self.client.get('/blog/create_post/')
         self.assertEqual(response.status_code, 200)
@@ -232,14 +232,14 @@ class TestView(TestCase) :
         self.assertTrue(Tag.objects.get(name='한글 태그'))
         self.assertEqual(Tag.objects.count(), 5)
 
+    # 포스트 수정 테스트 코드
     def test_update_post(self):
         update_post_url = f'/blog/update_post/{self.post_003.pk}/'
-
-        # 로그인하지 않은 경우
+        # 로그인 하지 않은 경우
         response = self.client.get(update_post_url)
         self.assertNotEqual(response.status_code, 200)
 
-        # 로그인은 했지만 작성자가 아닌 경우
+        # 로그인은 했지만, 작성자가 아닌 경우
         self.assertNotEqual(self.post_003.author, self.user_trump)
         self.client.login(
             username=self.user_trump.username,
@@ -261,12 +261,17 @@ class TestView(TestCase) :
         main_area = soup.find('div', id='main-area')
         self.assertIn('Edit Post', main_area.text)
 
+        tag_str_input = main_area.find('input', id='id_tags_str')
+        self.assertTrue(tag_str_input)
+        self.assertIn('파이썬 공부; python', tag_str_input.attrs['value'])
+
         response = self.client.post(
             update_post_url,
             {
-                'title': '세 번째 포스트를 수정했습니다.',
+                'title': '세 번째 포스트를 수정했습니다. ',
                 'content': '안녕 세계? 우리는 하나!',
-                'category': self.category_music.pk
+                'category': self.category_music.pk,
+                'tags_str': '파이썬 공부; 한글 태그, some tag'
             },
             follow=True
         )
@@ -275,3 +280,8 @@ class TestView(TestCase) :
         self.assertIn('세 번째 포스트를 수정했습니다.', main_area.text)
         self.assertIn('안녕 세계? 우리는 하나!', main_area.text)
         self.assertIn(self.category_music.name, main_area.text)
+
+        self.assertIn('파이썬 공부', main_area.text)
+        self.assertIn('한글 태그', main_area.text)
+        self.assertIn('some tag', main_area.text)
+        self.assertNotIn('python', main_area.text)
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
+from django.utils.text import slugify
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from .models import Post, Category, Tag
+from . models import Post, Category, Tag
 from django.core.exceptions import PermissionDenied
-from django.utils.text import slugify
 
 # Create your views here.
-class PostList(ListView):
+class PostList(ListView) :
     model = Post
 
     def get_context_data(self, **kwargs):
@@ -15,9 +15,8 @@ class PostList(ListView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
-
 # Post 상세 보기
-class PostDetail(DetailView):
+class PostDetail(DetailView) :
     model = Post
 
     def get_context_data(self, **kwargs):
@@ -25,7 +24,6 @@ class PostDetail(DetailView):
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
-
 
 # CreateView
 class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
@@ -57,16 +55,26 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
                     self.object.tags.add(tag)
 
             return response
-
+            # return super(PostCreate, self).form_valid(form)
         else:
             return redirect('/blog/')
 
 
 class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
-    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
     template_name = 'blog/post_update_form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PostUpdate, self).get_context_data()
+        if self.object.tags.exists():
+            tags_str_list = list()
+            for t in self.object.tags.all():
+                tags_str_list.append(t.name)
+            context['tags_str_default'] = '; '.join(tags_str_list)
+
+        return context
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated and request.user == self.get_object().author:
@@ -74,12 +82,32 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
         else:
             raise PermissionDenied
 
+    def form_valid(self, form):
+        response = super(PostUpdate, self).form_valid(form)
+        self.object.tags.clear()
 
-def category_page(request, slug):
-    if slug == 'no_category':
+        tags_str = self.request.POST.get('tags_str')
+        if tags_str:
+            tags_str = tags_str.strip()
+
+            tags_str = tags_str.replace(',', ';')
+            tags_list = tags_str.split(';')
+
+            for t in tags_list:
+                t = t.strip()
+                tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                if is_tag_created:
+                    tag.slug = slugify(t, allow_unicode=True)
+                    tag.save()
+                self.object.tags.add(tag)
+
+        return response
+
+def category_page(request, slug) :
+    if slug == 'no_category' :
         category = '미분류'
         post_lsit = Post.objects.filter(category=None)
-    else:
+    else :
         category = Category.objects.get(slug=slug)
         post_list = Post.objects.filter(category=category)
 
@@ -93,7 +121,6 @@ def category_page(request, slug):
             'category': category,
         }
     )
-
 
 def tag_page(request, slug):
     tag = Tag.objects.get(slug=slug)
