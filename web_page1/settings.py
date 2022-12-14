94224c8970c07bd7ddd254318b28d0a94c7c6efd
SINHUK <park070404@gmail.com>
2022-12-14 12:24:23 +0900

    도커에 장고만 담기

--- web_page1/settings.py
+++ web_page1/settings.py
@@ -23,12 +23,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$&t#q7$34t%aaoag3$yblk_e*&ny1mu*tke#nw2r^&)@-jm@2z"
+SECRET_KEY = os.environ.get('SECRET_KEY', "django-insecure-$&t#q7$34t%aaoag3$yblk_e*&ny1mu*tke#nw2r^&)@-jm@2z")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', 1))
 
-ALLOWED_HOSTS = []
+if os.environ.get('DJANGO-ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else:
+    ALLOWED_HOSTS = []
 
 
 # Application definition
